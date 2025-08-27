77cb401bd99ab4c4aaa43be27049773c10726845
JTaylor <jtaylor42@outlook.com>
2025-08-27 14:42:22 -0400

    Added get_character_count to stats.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_word_count
+from stats import get_character_count
 
 def get_book_text(file):
     with open(file) as f:
@@ -8,7 +9,7 @@ def get_book_text(file):
 
 def main(book):
     temp = get_book_text(book)
-    get_word_count(temp)
+    print(get_character_count(temp))
 
 
 main("./books/frankenstein.txt")
--- stats.py
+++ stats.py
@@ -3,3 +3,15 @@ def get_word_count(text):
     for words in text.split():
         num_words +=1
     print (f"{num_words} words found in the document")
+
+def get_character_count(text):
+    letters = {}
+
+    for character in text:
+        current = character.lower()
+        if current in letters:
+            letters[current] +=1
+        else:
+            letters[current] = 1
+    
+    return letters
