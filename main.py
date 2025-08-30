4b4c7784af9cc16ef753f6211d7df65470d1dba4
JTaylor <jtaylor42@outlook.com>
2025-08-30 15:44:05 -0400

    added comments

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+#import box
 from stats import get_word_count
 from stats import get_character_count
 from stats import book_report_num
 
+#Funcion to get contents of book
 def get_book_text(file):
+    #read the .txt file. I don't actually know how this works. Should reasearch.
     with open(file) as f:
         file_contents = f.read()
     return file_contents
 
 
+#Main function
 def main(book):
+    #tempory code to assign pass variables around. Clean later!
     temp = get_book_text(book)
     temp2 = get_character_count(temp)
     print(book_report_num(temp2))
--- stats.py
+++ stats.py
@@ -1,21 +1,34 @@
 def get_word_count(text):
+    #initalize empty word count
     num_words = 0
+
+    #iterate text
     for words in text.split():
+        #update quantity
         num_words +=1
+    
+    #output quantity
     print (f"{num_words} words found in the document")
 
 def get_character_count(text):
+    #create empty dictionary
     letters = {}
 
+    #iterate through text
     for character in text:
+        #lowercase letters only
         current = character.lower()
+
+        #update quantity, add if it's not there
         if current in letters:
             letters[current] +=1
         else:
             letters[current] = 1
     
+    #return the dictionary
     return letters
 
+#Start here on lesson
 def book_report_num(unsorted_report):
     n_report = []
     for char, num in unsorted_report.items():
@@ -23,6 +36,14 @@ def book_report_num(unsorted_report):
             n_report.append({"char": char, "num" : num})
     n_report.sort(reverse=True, key=lambda item: item["num"])
 
-        
-            
     return n_report
+
+
+
+# tip from Boots
+#   in main.py, print the headers.
+#   Print total word count.
+#   Loop your sorted list and print lines like:
+#
+#       for item in sorted_chars:
+#           print(f"{item['char']}: {item['num']}")
